c8edfcc8e13f8069844f96392e35a2ae21a84125
LightbulbProductions <gerbermichelle23@gmail.com>
2017-06-26 05:07:42 -0400

    more portfolio basics

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+4.times do |skill|
+  Skill.create!(
+  title: "Rails #{skill}",
+  percent_utilized: 25
+  )
+end
+
+puts "4 skills created"
+
+8.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My portfolio subtitle",
+    body: "Ea mixtape tbh synth man braid, chambray intelligentsia tattooed lyft gastropub.Tumblr helvetica paleo 
+      neutra mumblecore. Organic live-edge snackwave lumbersexual viral polaroid, excepteur vinyl photo booth aute
+      pork belly literally fugiat 3 wolf moon. Woke tumeric brooklyn +1 nostrud, deserunt hexagon offal roof party
+      chartreuse scenester pork belly. Esse next level id chambray crucifix elit kombucha dolore cupidatat 8-bit 
+      pariatur before they sold out selvage leggings.",
+      main_image:"http://placehold.it/600x400",
+      thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "8 portfolio items created"
