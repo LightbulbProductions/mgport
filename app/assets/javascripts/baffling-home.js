5869b82b9c735fbb5c5541672b3b4f4a2a7c5134
LightbulbProductions <gerbermichelle23@gmail.com>
2017-07-31 13:51:54 -0400

    style changes for about me

--- app/assets/javascripts/baffling-home.js
+++ app/assets/javascripts/baffling-home.js
@@ -1,8 +1,14 @@
-var b = window.baffle('cover-heading', 
+  var b = window.baffle('h1', 
+    {
+      characters: 'b6c7807bb10b5d867000',// ▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒', 
+    speed: 50
+  });
+  b.start().once().reveal(2000, 1000);
+  
+var b2 = window.baffle('h3',
   {
-    characters: 'b6c7807bb10b5d867000',// ▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒', 
+    characters: 'b6c7807bb10b5d867000jskssjaoidjfiefaoiefjoisdefjoi;dsfj',// ▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒', 
   speed: 50
 });
-b.start().once().reveal(500, 500);
-
-
+b2.start().once().reveal(4000, 2000);
+  
